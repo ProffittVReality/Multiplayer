cad819bc4df97becc32b9c3c841aebf4fccae559
Proffitt <uvaperceptionlab@gmail.com>
2017-07-11 19:23:34 -0400

    New feature: oculus moves along with bike (shaky graphics)

---
+++ Assets/_Scripts/RiderScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiderScript : MonoBehaviour {
+
+    GameObject chariot;
+    public GameObject cameraRig;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.Find("Chariot(Clone)") != null)
+        {
+            chariot = GameObject.Find("Chariot(Clone)");
+            //print(chariot.transform.position);
+            cameraRig.transform.position = chariot.transform.position;
+        }
+    }
+}
