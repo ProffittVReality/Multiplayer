364b51939f4b952a6e81ded3ccfe5f4d342bc686
Ryan <uvaperceptionlab@gmail.com>
2017-07-13 16:22:40 -0400

    Vive Rider Script

---
+++ Assets/_Scripts/ViveRiderScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveRiderScript : MonoBehaviour {
+
+	GameObject chariot;
+	GameObject head;
+	GameObject left;
+	GameObject right;
+	bool isAttached = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!isAttached && GameObject.Find("Chariot(Clone)") != null)
+		{
+			chariot = GameObject.Find("Chariot(Clone)");
+			head = GameObject.Find("Avatar(Clone)");
+			left = GameObject.Find("LHand(Clone)");
+			right = GameObject.Find("RHand(Clone)");
+			head.transform.SetParent(chariot.transform);
+			left.transform.SetParent(chariot.transform);
+			right.transform.SetParent(chariot.transform);
+			isAttached = true;
+		}
+	}
+}
