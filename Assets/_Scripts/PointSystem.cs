3ca251d1c9288316a4bd90ac18556572e6c9d3e2
Ryan <uvaperceptionlab@gmail.com>
2017-07-18 17:42:26 -0400

    Point system improvements

--- Assets/_Scripts/PointSystem.cs
+++ Assets/_Scripts/PointSystem.cs
@@ -7,36 +7,52 @@ public class PointSystem : MonoBehaviour {
 	int points;
 	int timeRemaining;
 
+	PhotonView photonView;
+
 	// Use this for initialization
 	void Start () {
 		points = 0;		
+		photonView = PhotonView.Get (this);
 	}
 
 	void Update() {
 		print (points);
 		if (Input.GetKeyDown ("space")) {
-			PhotonView photonView = PhotonView.Get (this);
-			photonView.RPC ("AddPoints", PhotonTargets.All, 1);
+			photonView.RPC ("PunAddPoints", PhotonTargets.All, 1);
 		}
 	}
 
+	// local method
+	public void AddPoints(int amnt) {
+		photonView.RPC ("PunAddPoints", PhotonTargets.All, amnt);
+	}
+
+	// local method
+	public void SubtractPoints(int amnt) {
+		photonView.RPC ("PunSubtractPoints", PhotonTargets.All, amnt);
+	}
+
+	// method to update all point managers
 	[PunRPC]
-	void AddPoints(int amnt) {
+	void PunAddPoints(int amnt) {
 		points += amnt;
 	}
 
+	// method to update all point managers
 	[PunRPC]
-	public void SubtractPoints(int amnt) {
+	public void PunSubtractPoints(int amnt) {
 		points -= amnt;
 		// cannot have negative points
 		if (points < 0)
 			points = 0;
 	}
 
+	// local getter
 	public int GetPoints() {
 		return points;
 	}
 
+	// local getter
 	public int GetSecondsRemaining() {
 		return timeRemaining;
 	}
